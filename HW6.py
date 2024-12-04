df8ec39fd24f03696812b2e302167ee5c455aaa2
DEDMOZAII <GentelmanZocker@Gmail.com>
2024-12-04 16:25:12 +0200

    final

---
+++ HW6.py
@@ -0,0 +1,68 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, value):
+        if not value:
+            raise ValueError("Ім'я не може бути пустим")
+        super().__init__(value)
+
+class Phone(Field):
+    def __init__(self, value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Номер телефону повининен складатись з 10 цифр")
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+    
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+         
+    def remove_phone(self, phone):
+        phone_to_remove = self.find_phone(phone)
+        if phone_to_remove:
+            self.phones.remove(phone_to_remove)
+        else:
+             raise ValueError(f"номер {phone} не знайдено")
+        
+    def edit_phone(self, old_phone, new_phone):
+        phone_to_edit = self.find_phone(old_phone)
+        if phone_to_edit:
+            self.phones.remove(phone_to_edit)
+            self.add_phone(new_phone)
+        else:
+            raise ValueError(f"номер {old_phone} не знайдено")
+        
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p 
+        return None
+            
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+        else:
+            raise ValueError(f"Record with name {name} not found.")
+
+    def __str__(self):
+        return "\n".join(str(record) for record in self.data.values())
